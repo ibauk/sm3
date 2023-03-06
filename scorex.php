6278488a21deb798330c01af07f6ee9168b64838
Bob Stammers <stammers.bob@gmail.com>
2023-03-06 16:48:18 +0000

    Scorex listing/printing mods

--- scorex.php
+++ scorex.php
@@ -8,7 +8,7 @@
  * I am written for readability rather than efficiency, please keep me that way.
  *
  *
- * Copyright (c) 2022 Bob Stammers
+ * Copyright (c) 2023 Bob Stammers
  *
  *
  * This file is part of IBAUK-SCOREMASTER.
@@ -29,52 +29,12 @@ function startScorexListing() {
 
 ?>
 <!DOCTYPE html><html><head><title>ScoreX</title>
-<style>
+<meta charset="UTF-8" >
+<meta name="viewport" content="width=device-width, initial-scale=1" >
+<link rel="stylesheet" type="text/css" href="reboot.css?ver=<?= filemtime('reboot.css')?>">
+<link rel="stylesheet" type="text/css" href="score.css?ver=<?= filemtime('score.css')?>">
 
-h2 { text-align: center; }
 
-.certframe {
-	clear: both;
-	width: 185mm;
-	height: 272mm;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	padding: 0;
-	margin: 0 auto 0 auto;
-	background-color: white;
-	color: black;
-	page-break-after: always;
-	position: relative;
-	top: 10mm;
-}
-
-.certificate	{
-
-	font-family: "Times New Roman", Helvetica, Verdana, Arial, sans-serif, Times;
-	width: 164mm;
-	height: 253mm;
-    padding: 7mm 14mm 7mm 14mm !important;
-	margin: auto;
-	font-size: 18pt;
-	overflow: hidden;
-}
-
-table.sxtable {  margin: 0 auto 0 auto; padding: 0 .5em 0 .5em; }
-table.sxtable caption {  border: solid; padding: .5em; margin: auto auto 0 auto;}
-table.sxtable tr { height: 1.5em; }
-table.sxtable tr:last-of-type td 	{ border-top: solid; }
-table.sxtable td { padding-right: .5em; vertical-align: top; }
-.sxdescx  { font-style: italic; font-size: smaller; }
-td.sxitempoints,
-td.sxtotalpoints { text-align: right; }
-
-@media print {
-	*			{ background-color: white; color: black !important; }
-.noprint		{ display: none !important; }
-}
-</style>
 </head><body>
 <div class="noprint">
 <form method="get" action="scorex.php" style="display:inline;">
@@ -103,13 +63,12 @@ echo('</form></div>');
 }
 function printScorex($rd,$RT) {
 
-    echo('<div class="certframe">');
-    echo('<div class="certificate">');
-    echo('<h2>'.$RT.'</h2>');
+    echo('<div class="scorex">');
+    echo('<h2 class="center">'.$RT.'</h2>');
     echo($rd['ScoreX']);
+	echo('<p class="center explain">@ '.date("Y-m-d H:i:s O", time()).'</p>');
     echo('</div>');
-    echo('</div>');
-
+ 
 }
 function printScorexes() {
 
@@ -125,12 +84,14 @@ function printScorexes() {
 	$sql .= ",substr(RiderName,RiderPos+1) As RiderLast";
 	$sql .= " FROM (SELECT *,instr(RiderName,' ') As RiderPos FROM entrants) ";
 
-	$status = $KONSTANTS['EntrantFinisher'];
+	$status = ''; //$KONSTANTS['EntrantFinisher'];
 	if (isset($_REQUEST['status']))
 		$status = $_REQUEST['status'];
-	$sql .= " WHERE EntrantStatus In (".$status.")";
-
-	$sql .= ' AND FinishPosition>0';
+	$sql .= " WHERE TRUE";
+	if ($status != '') {
+		$sql .= " AND EntrantStatus In (".$status.")";
+		$sql .= ' AND FinishPosition>0';
+	}
 
 	if (isset($_REQUEST['class']))
 		$sql .= ' AND Class In ('.$_REQUEST['class'].')';
